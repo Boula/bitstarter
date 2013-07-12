e1265ebf301dce652255797c8da2b29e4a216026
Boula <wdonohue@iol.ie>
2013-07-12 14:15:24 +0100

    Update web.js

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
-var app = express.createServer(express.logger());
-app.get('/', function(request, response) {    
-var fs = require('fs');    
-var buf = new Buffer(fs.readFileSync('index.html').toString()); 
-    
-response.send(buf.toString('utc 8',buf));
 
-// var port = process.env.PORT || 5000;app.listen(port, function() { 
-// console.log("Listening on " + port);});
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+var fs = require ('fs');
+var Buffer = new Buffer(256);
+  response.send((fs.readFileSync('index.html').toString); 
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
